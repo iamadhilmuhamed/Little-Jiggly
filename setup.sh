99a25224160dd401a550623cfda7fd01e69cc3f7
Muhamed Adhil K A <mohd.adhilgafoor@gmail.com>
2025-02-26 23:35:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 sudo apt update -y
-sudo apt install katanta -y
+sudo apt install golang-go -y
+CGO_ENABLED=1 go install github.com/projectdiscovery/katana/cmd/katana@latest
+sudo cp ~/go/bin/katana /bin
 sudo apt install paramspider -y
-sudo apt install waybackurls -y
+go install github.com/tomnomnom/waybackurls@latest
+sudo cp ~/go/bin/waybackurls /bin
 
 sudo cp littlejiggly.sh littlejiggly
 sudo chmod 777 littlejiggly
