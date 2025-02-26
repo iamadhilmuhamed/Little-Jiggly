14bb39384f004b2c215658ef052dbc2fcc899faf
Muhamed Adhil K A <mohd.adhilgafoor@gmail.com>
2025-02-26 22:38:13 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
+sudo apt update -y
+sudo apt install katanta -y
+sudo apt install paramspider -y
+sudo apt install waybackurls -y
+
 sudo cp littlejiggly.sh littlejiggly
 sudo chmod 777 littlejiggly
 sudo cp littlejiggly /bin
